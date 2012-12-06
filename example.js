70c42ceeade51bd3e7eae4177d30bb2fb2a1c238
Peng Xie <xiepeng.peng@gmail.com>
2012-12-06 19:32:46 +0000

    example of calculating a treehash from a file stream.

--- example.js
+++ example.js
@@ -1,10 +1,22 @@
-var treehash = require('./lib/treehash.js');
+var fs = require('fs'),
+    treehash = require('./lib/treehash.js');
 
+console.info('\n\nLoading the entire README.md into memory...');
 var buf = require('fs').readFileSync('README.md');
 console.info("The treehash for file README.md:\n" + treehash.getTreeHashFromBuffer(buf));
 
 var thStream = treehash.createTreeHashStream ();
-thStream.update(buf);
-console.info("The treehash for file README.md:\n" + thStream.digest());
-console.info("The treehash for file README.md:\n" + thStream.digest()); // Exception from a second digestion.
+
+console.info('\n\nLoading README.md as a stream...');
+var readStream = fs.createReadStream('README.md');
+readStream.on('data', function(chunk) {
+    console.info('Updating treehash...');
+    thStream.update(chunk);
+});
+
+readStream.on('end', function (){
+    console.info("The treehash for file README.md:\n" + thStream.digest());
+    // console.info("The treehash for file README.md:\n" + thStream.digest()); // Exception from a second digestion.
+});
+
 
